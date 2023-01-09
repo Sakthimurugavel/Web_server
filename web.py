e471bf6e7f0e38e72a157a25b251bf59b682ae09
sakthimurugavel <sakthimurugavel2020@gmail.com>
2023-01-09 15:42:43 +0000

    success

---
+++ web.py
@@ -0,0 +1,24 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+content = """
+<html>
+<head>
+</head>
+<body>
+<h1>Name: m.sakthivel </h1>
+<h1>reference no: 22007765 WW </h1>
+</body>
+</html>
+"""
+  
+class myserver1(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("content-type", 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(content.encode())
+
+
+Server_address =('',80)
+httpd= HTTPServer(Server_address,myserver1)
+httpd.serve_forever()
